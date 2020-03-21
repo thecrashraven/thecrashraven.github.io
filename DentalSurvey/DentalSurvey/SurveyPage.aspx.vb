9e2b85cfdc4b6341c11ababec13bcd96dfd44fec
scott.farney <scott.farney@WCH-E5580-21>
2020-03-21 18:36:53 -0500

    survey tst

--- DentalSurvey/DentalSurvey/SurveyPage.aspx.vb
+++ DentalSurvey/DentalSurvey/SurveyPage.aspx.vb
@@ -1,8 +1,20 @@
-﻿Public Class SurveyPage
+﻿Imports System.Data.SqlClient
+
+Public Class SurveyPage
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+    End Sub
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        Dim Command As New SqlCommand("usp_InsertSurveyData")
+        Command.CommandType = CommandType.StoredProcedure
+        Command.Parameters.Add("@ChartNum", SqlDbType.VarChar, 10).Value = Chart_Number.Text
+        Command.Parameters.Add("@Ethnicity", SqlDbType.Int).Value = Ethnicitiy.Text
+        Command.Parameters.Add("@Gender", SqlDbType.Int).Value = GenderList.Text
+        Command.Parameters.Add("@ZipCode", SqlDbType.Int).Value = Zip.Text
+        Command.Parameters.Add("@Birthday", SqlDbType.SmallDateTime).Value = BirthdayList.Text
     End Sub
 
 End Class
