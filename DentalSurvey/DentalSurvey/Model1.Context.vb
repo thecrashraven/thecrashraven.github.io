d2662e4afa452426700f3169faf4384d55122802
scott.farney <scott.farney@WCH-E5580-21>
2020-03-24 18:36:16 -0500

    update database, add tables for clinic cites, dental codes and dental code costs

--- DentalSurvey/DentalSurvey/Model1.Context.vb
+++ DentalSurvey/DentalSurvey/Model1.Context.vb
@@ -28,6 +28,9 @@ Partial Public Class DentalSurveyEntities
     Public Overridable Property Gender_Codes() As DbSet(Of Gender_Codes)
     Public Overridable Property Surveys() As DbSet(Of Survey)
     Public Overridable Property vw_Survey() As DbSet(Of vw_Survey)
+    Public Overridable Property Clinic_Sites() As DbSet(Of Clinic_Sites)
+    Public Overridable Property Dental_Code_Costs() As DbSet(Of Dental_Code_Costs)
+    Public Overridable Property Dental_Codes() As DbSet(Of Dental_Codes)
 
     Public Overridable Function dbo_usp_GetSurveyData() As ObjectResult(Of dbo_usp_GetSurveyData_Result)
         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of dbo_usp_GetSurveyData_Result)("dbo_usp_GetSurveyData")
@@ -59,6 +62,15 @@ Partial Public Class DentalSurveyEntities
         Dim birthdayParameter As ObjectParameter = If(birthday.HasValue, New ObjectParameter("Birthday", birthday), New ObjectParameter("Birthday", GetType(Date)))
 
         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction("usp_InsertSurveyData", chartNumParameter, ethnicityParameter, genderParameter, zipCodeParameter, birthdayParameter)
+    End Function
+
+    <DbFunction("DentalSurveyEntities", "fn_GetDentalCodes")>
+    Public Overridable Function fn_GetDentalCodes() As IQueryable(Of fn_GetDentalCodes_Result)
+         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.CreateQuery(Of fn_GetDentalCodes_Result)("[DentalSurveyEntities].[fn_GetDentalCodes]()")
+    End Function
+
+    Public Overridable Function usp_GetSurveyData() As ObjectResult(Of usp_GetSurveyData_Result)
+        Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of usp_GetSurveyData_Result)("usp_GetSurveyData")
     End Function
 
 End Class
